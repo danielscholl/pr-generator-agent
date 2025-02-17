71b1277a71d238fc804b7616a70aabab05a87816
Daniel Scholl <daniel.scholl@microsoft.com>
2025-02-16 18:31:22 -0600

    fix: update release-please configuration for better version management

--- aipr/__init__.py
+++ aipr/__init__.py
@@ -2,6 +2,6 @@
 AIPR - AI-powered Merge Request Description Generator
 """
 
-# x-release-please-start-version
+# x-release-please-version
 __version__ = "0.1.1"
-# x-release-please-end-version
+# x-release-please-end
